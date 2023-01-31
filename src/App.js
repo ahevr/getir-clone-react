f2e955164402103bb51dcb3f5840c11596c03cc5
ahevr <ahmethusreversen@hotmail.com>
2023-01-31 18:12:36 +0300

    slider eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from 'components/Header'
+import HeroSection from 'components/HeroSection'
 import React from 'react'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <HeroSection/>
     </div>
   )
 }
